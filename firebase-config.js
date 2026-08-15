4335f863504acb7d00519e3b370c05bcf118a555
jeandamourumuhoza24-del <jeandamourumuhoza24@gmail.com>
2026-08-15 12:36:32 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase configuration for Moto Progress Rwanda
-export const firebaseConfig = {
-  apiKey: "AIzaSyAxaQIfr8Xa_u_kbuuPSsXxdMpCxDcwFBg",
-  authDomain: "moto-progress-rwanda.firebaseapp.com",
-  projectId: "moto-progress-rwanda",
-  storageBucket: "moto-progress-rwanda.firebasestorage.app",
-  messagingSenderId: "618982978698",
-  appId: "1:618982978698:web:971baeddb9987c770a6973"
-};
