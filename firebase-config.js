d00fde472758423ea34c8f6743c32e0362102d87
jeandamourumuhoza24-del <jeandamourumuhoza24@gmail.com>
2026-08-15 15:30:37 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase configuration for Moto Progress Rwanda
+export const firebaseConfig = {
+  apiKey: "AIzaSyB6N26QBVVbi-r39_qb7hnTbn9JeZSQhr4",
+  authDomain: "moto-progress-rwanda.firebaseapp.com",
+  projectId: "moto-progress-rwanda",
+  storageBucket: "moto-progress-rwanda.firebasestorage.app",
+  messagingSenderId: "618982978698",
+  appId: "1:618982978698:web:971baeddb9987c770a6973"
+};
